4dcaaa4123fbbd6584a4d86b9a202d5c0f3914cd
Adams Albarka <abdullahiadam@student.oauife.edu.ng>
2022-07-23 00:23:46 +0100

    About Page Commit
    The about page will be completed today 23/July/2022

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './about.css'
+import { data } from '../../data'
+import Button from '../../tools/Button/Button'
+import { BsFillJournalBookmarkFill } from 'react-icons/bs'
 export default function About() {
   return (
     <section className='about' id='about'>
-      Hello About
+      <h5>Get to know Herr Adams</h5>
+      <h1>I write and speak 5 Languages</h1>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ data.imag2 } alt="" />
+          </div>
+        </div>
+
+      <div className="about__content">
+        <div className="about__cards">
+          <article className="about__card">
+            <BsFillJournalBookmarkFill className='about__icon' />
+            <h5>Experience</h5>
+            <small>3 + Years</small>
+          </article>
+        </div>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+          Aperiam, deleniti in quasi incidunt assumenda ea eum? Numquam error amet accusamus, 
+          voluptate laudantium, nulla, saepe excepturi aliquid ipsum illum ipsam magni.
+        </p>
+        
+        <Button>
+          <a href="#contact" className='btn btn-primary'>
+            Let's Talk
+          </a>
+        </Button>
+
+      </div>
+
+      </div>
     </section>
   )
 }
--- src/data.js
+++ src/data.js
@@ -1,5 +1,7 @@
 import ME from './assets/me.png'
+import MeAbout from "./assets/me-about.jpg";
 
 export const data = {
     imag: ME,
+    imag2: MeAbout,
 }
