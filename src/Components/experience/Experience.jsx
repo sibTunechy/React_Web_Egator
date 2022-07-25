de2ce414ca7d7e4c6951ae712d1818e950b01706
Adams Albarka <abdullahiadam@student.oauife.edu.ng>
2022-07-25 08:15:46 +0100

    Experience Component
    Development in Progress

--- src/Components/experience/Experience.jsx
+++ src/Components/experience/Experience.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import './experience.css'
+import { BsPatchCheckFill } from 'react-icons/bs'
+import { expFrontEndData, expBackEndData } from '../../data'
 export default function experience() {
   return (
-    <section className='experience' id='experience'>
-      Hello Experience
-    </section>
+    <section id='experience'>
+      <h5>GENERAL SKILLS</h5>
+      <h2>My Experiences</h2>
+
+      <div className="container experience__container">
+          <div className="experience__frontend">
+          <h3>FRONTEND DEVELOPMENT</h3>
+          <div className="experience__content">
+        {expFrontEndData.map((item) =>(
+            <article className="experience__details">
+              <span className='experience__details-icon'>{item.icon}</span>
+              <h4>{item.progLang}</h4>
+              <div>
+              <small className='text-light'>{item.experience}</small>
+              <small className="text-light">{item.framewrk}</small>
+              </div>
+            </article>
+            ))}
+          </div>
+        </div>
+        <div className="experience__backend">
+        <h3>BACKEND DEVELOPMENT</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+            <BsPatchCheckFill className='experience__details-icon' />
+              <h4>HTML</h4>
+              <div>
+              <small className='text-light'>Experienced</small>
+              <small className="text-light"></small>
+              </div>
+            </article>
+          </div>
+        </div>
+        </div>
+  </section>
   )
 }
--- src/data.js
+++ src/data.js
@@ -1,6 +1,7 @@
 import ME from './assets/me.png'
 import MeAbout from "./assets/me-about.jpg";
 import { IoLanguageSharp } from 'react-icons/io5'
+import { BsPatchCheckFill } from 'react-icons/bs'
 import { FaAward, FaChalkboardTeacher } from 'react-icons/fa'
 import { GiTeacher } from 'react-icons/gi'
 
@@ -14,3 +15,52 @@ export const cardData = [
     { icon: <FaAward />, title: "React Development", year: "2 + Years" },
     { icon: <GiTeacher />, title: "Teaching Skill", year: "4 + Years" },
   ];
+
+export const expFrontEndData = [
+    {
+        icon: <BsPatchCheckFill />, 
+        progLang: "HTML", 
+        experience: "Well-Experienced" 
+    },
+    {
+        icon: <BsPatchCheckFill />, 
+        progLang: "CSS", 
+        experience: "Well-Experienced",
+        framewrk: "Bootstrap & Tailwind"
+    },
+    {
+        icon: <BsPatchCheckFill />, 
+        progLang: "JS", 
+        experience: "Advanced",
+        framewrk: "ReactJS"
+    },
+
+]
+
+export const expBackEndData = [
+    {
+        icon: <BsPatchCheckFill />, 
+        progLang: "Python", 
+        experience: "Basics" 
+    },
+    {
+        icon: <BsPatchCheckFill />, 
+        progLang: "MYSQL", 
+        experience: " Intermediate " 
+    },
+    {
+        icon: <BsPatchCheckFill />, 
+        progLang: "NodeJs", 
+        experience: "Experienced" 
+    },
+    {
+        icon: <BsPatchCheckFill />, 
+        progLang: "MongoDB", 
+        experience: "Experienced" 
+    },
+    {
+        icon: <BsPatchCheckFill />, 
+        progLang: "PHP", 
+        experience: "Basics" 
+    },
+]
