45663204ae40dceb2bb1f838963906375e68fb6c
Adams Albarka <abdullahiadam@student.oauife.edu.ng>
2022-07-26 08:02:00 +0100

    Services Component
    Completion of Services Component

--- src/Components/services/Services.jsx
+++ src/Components/services/Services.jsx
@@ -1,10 +1,59 @@
 import React from 'react'
 import './services.css'
+import { BiCheck } from "react-icons/bi"
+import { servicesListWrite, 
+         servicesListTeach,
+         servicesListWeb } 
+from '../../data'
 
 export default function Services() {
   return (
-    <section className='services' id='services'>
-      Hello Services
+    <section id='services'>
+     <h5>What I Offer</h5>
+     <h2>My Services</h2>
+     <div className="container services__container">
+      <article className='services'>
+        <div className="service__head">
+          <h3>Writing</h3>
+        </div>
+    {servicesListWrite.map((item) => (
+        <ul className="service__list">
+          <li>
+            <span className="service__list-icon">{item.icon}</span>
+            <p>{item.text}</p>
+          </li>
+        </ul>
+    ))}
+      </article>
+
+      <article className='services'>
+        <div className="service__head">
+          <h3> Web Programming </h3>
+        </div>
+    {servicesListWeb.map((item) => (
+        <ul className="service__list">
+          <li>
+            <span className="service__list-icon">{item.icon}</span>
+            <p>{item.text}</p>
+          </li>
+        </ul>
+    ))}
+      </article>
+
+      <article className='services'>
+        <div className="service__head">
+          <h3>Language Teaching</h3>
+        </div>
+    {servicesListTeach.map((item) => (
+        <ul className="service__list">
+          <li>
+            <span className="service__list-icon">{item.icon}</span>
+            <p>{item.text}</p>
+          </li>
+        </ul>
+    ))}
+      </article>
+     </div>
     </section>
   )
 }
